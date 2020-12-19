4ef7323a0a7a44881e4ef030dde4c81b9f4be730
unknown <ndnh2001@gmail.com>
2020-12-19 10:06:08 +0700

    Tieng chim ne

--- mini_git/src/mini_git/Bird.java
+++ mini_git/src/mini_git/Bird.java
@@ -4,7 +4,7 @@ package mini_git;
 public class Bird extends animal implements sound{
     @Override
     public String voice() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+     return "Sound of Bird ";
     }
     
 }
